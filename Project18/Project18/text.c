6b548a1a53c558378904d48b29b50057c3f3c9f2
cfkcoder <1261728635@qq.com>
2020-12-14 12:16:44 +0800

    12.14

---
+++ Project18/Project18/text.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+typedef int  Elemtype;
+
+typedef struct DCListNode{
+
+	Elemtype data;
+	struct DCListNode *prev;
+	struct DCListNode *next;
+
+}DClistNode;
+
+
+typedef struct DCListNode* DCList;
+
+int main()
+{
+	
+
+
+
+
+
+	return 0;
+}
