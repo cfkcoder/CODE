545b7a6c011ff32d2d8dd5914c561266adb7a352
cfkcoder <1261728635@qq.com>
2021-01-16 22:53:56 +0800

    1.16

---
+++ Project49/Project49/text.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+//
+//int main()
+//{
+//
+//	int total = 20;
+//	int empty = total;
+//	int money = 20;
+//
+//
+//	while (empty > 1)
+//	{
+//		total += empty / 2;
+//		empty = empty / 2 + empty % 2;
+//
+//	}
+//	
+//	printf("%d\n", total);
+//
+//	return 0;
+//}
+
+
+
+	//int main()
+	//{
+
+
+	//	int *p = NULL;
+	//	int arr[10] = { 0 };
+	//	p = &arr;
+	//	return 0;
+	//}
+
+
+
+void Game()
+{
+
+	return;
+}
+
+int main()
+{
+	Game();
+
+
+	return 0;
+}
+
+
