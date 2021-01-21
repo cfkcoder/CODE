0166011970ffc7268f3ae1a96cda56956f643a63
cfkcoder <1261728635@qq.com>
2021-01-21 20:31:26 +0800

    1.21

---
+++ Project55/Project55/text.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+
+//
+//
+//void bubblesort(int arr[], int n)        //c语言的参数中没有数组的存在，形式是数组，但实际上是数组首元素的地址(即指针），因为c语言极度追求效率，不允许真正传的是数组.
+//{
+//	int end = n;
+//
+//	while (end > 0)
+//	{
+//		  int i = 1;
+//		  for (; i < end; i++)
+//		 {
+//			if (arr[i - 1] > arr[i])
+//			{
+//				int temp = arr[i - 1];
+//				arr[i - 1] = arr[i];
+//				arr[i] = temp;
+//
+//
+//			}
+//		 }
+//		  --end;
+//	}
+//
+//
+//	
+//
+//}
+
+//
+//
+//int main()
+//{
+//	//单双精度浮点数后取的位数
+//	int n1 = 10.0;
+//	printf("%d ", n1);
+//
+//	int arr[] = { 1, 2, 3, 30, 40, 5, 44, 21 };
+//	int n = sizeof(arr) / sizeof(arr[0]);
+//	//bubblesort(arr, n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//		/*int a=1, b=1;
+//		printf("%d %d", a,b);*/
+//		/*char arr[] ="asdffggg";
+//		char *p = arr;
+//		char *p1 = arr;
+//		printf("%c\n", *(p++));
+//		printf("%c\n", *++p1);*/
+//
+//		/* char a = '1';
+//		 printf("%c\n", a);
+//		 return 0;*/
+//
+//
+//		 return 0;
+//
+//}
+
+
+
+int main()
+{
+	int i = 0;
+	printf("%d ", i = 10);
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
