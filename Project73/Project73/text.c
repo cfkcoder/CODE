9911e5be9772d81ae730be79e800f2d95c14c9a2
cfkcoder <1261728635@qq.com>
2021-02-04 19:39:49 +0800

    2.4

---
+++ Project73/Project73/text.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+
+int main()
+{
+
+	char a[1000] = {1,2,3,45,0,0,0,0,0,0,0,0,0};
+	int n = strlen(a);
+	char b = -256;
+	printf("%d\n", b);
+
+
+
+
+	return 0;
+
+}
