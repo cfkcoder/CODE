b034ac0913f813c138daea329cfc4d9d98ccc01f
cfkcoder <1261728635@qq.com>
2021-02-15 09:21:36 +0800

    operator（1）

---
+++ Project81/Project81/text.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+#define N 10
+
+int main()
+{
+
+
+
+	int x;
+	scanf("%d", &x);
+	if (x-- < 5)
+	{
+		printf("%d\n", x);
+	}
+	else
+	{
+		printf("%d\n", x++);
+	}
+	/*int j=2;
+	k = 1;
+
+
+	printf("%d\n", i);
+	printf("%d", k);
+*/
+    
+
+
+
+	/*int i = 10;
+	printf("%d %d %d", i++, i++, i++);
+	printf("\n");
+
+	i = 10;
+	printf("%d %d %d", ++i, ++i, ++i);*/
+
+
+
+
+	/*int i = 0, a = 0, b = 2, c = 3, d = 4;
+	i = a++&&++b&&d++;
+	printf(" a=%d\n b=%d\n c=%d\n d=%d\n ", a, b, c, d);
+	printf("%d", i);*/
+
+	//int i = 0, a = 0, b = 2, c = 3, d = 4;
+	//i = a++ || ++b || d++;
+	//printf(" a=%d\n b=%d\n c=%d\n d=%d\n ",a,b,c,d);
+	//printf("%d\n", i);
+
+ //   
+
+
+
+
+
+	return 0;
+}
