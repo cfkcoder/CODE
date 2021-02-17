6d1e865a271426015e673bbfa4b1a617e6f5cafd
cfkcoder <1261728635@qq.com>
2021-02-17 16:39:27 +0800

    unsigned int i

---
+++ Project83/Project83/text.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main()
+{
+
+
+	unsigned int i = 0;
+	int n = 10;
+	while (n >=i)
+	{
+
+		printf("%d\n", n);
+		n--;
+
+	}
+
+
+	return 0;
+}
