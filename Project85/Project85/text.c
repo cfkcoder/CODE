240e166bed57afedadd95c9c1522bfa43bfbc8d2
cfkcoder <1261728635@qq.com>
2021-02-19 22:23:09 +0800

    Comma expression

---
+++ Project85/Project85/text.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+
+//
+//
+//void Test( char *str)
+//{
+//
+//	printf("%s\n", str);
+//
+//
+//
+//
+//}
+
+int main()
+{
+
+	//int a = 1;
+	//int b = 2;
+	//int c = (a > b, a = b+ 10, a, b = a + 1);
+	//printf("%d\n", c);
+
+
+	//int a;
+	//int b;
+	//int count = 0;
+	//for (a = 1, b = 4; a >= 0, b; a--, b--)
+	//{
+
+	//	count++;
+
+	//}
+
+	//printf("%d\n", count);
+
+
+	//int a[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	//a[7] = 66;
+	//printf("%d\n", a[7]);
+
+	/*char *str = "hellow world!";
+	Test(str);
+*/
+
+	int a = 1;
+	int b = 2;
+	a = b = 3;
+	printf("%d %d\n", a, b);
+	return 0;
+
+}
