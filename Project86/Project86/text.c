3cbe7dbb6a12fdd9a3296a8ec0f1f601711393b7
cfkcoder <1261728635@qq.com>
2021-02-20 10:10:20 +0800

    Expression return value

---
+++ Project86/Project86/text.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+
+int main()
+{/*
+	int a = 4;
+	if (a > 0)
+	{
+		if (a > 5)
+		{
+			printf("a>5");
+		}
+	}
+
+	else {
+		printf("kkk");
+	}
+
+
+*/
+
+
+	//int *p=(0x66);
+
+	//
+
+
+	//*p = 20;
+	//printf("%d\n", *p);
+
+	int a = 2;
+	if (a % 2)
+	{
+		printf("1");
+	}
+	else
+	{
+		printf("1111");
+	}
+	return 0;
+}
