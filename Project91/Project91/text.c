62c12532c7bbbb5af5e20cefd08348539296b15a
cfkcoder <1261728635@qq.com>
2021-03-01 10:34:28 +0800

    getchar

---
+++ Project91/Project91/text.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+
+int main()
+{
+
+
+	//char a = 's';
+	////char a;
+	//char b;
+	//b = getchar(a);
+
+	//printf("%c ", a);
+	////putchar(a);
+	//printf("\n");
+
+
+	char a;
+	a = getchar();
+	printf("%c \n", a);
+
+
+	return 0;
+}
