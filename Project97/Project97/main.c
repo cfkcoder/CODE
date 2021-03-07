faebc06db8f50b4f77518563b581f189ade457c3
cfkcoder <1261728635@qq.com>
2021-03-07 10:27:16 +0800

    extern a; (void)msg()

---
+++ Project97/Project97/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+
+char a = 'A';
+
+void msg()
+{
+
+	printf("Hello\n");
+
+}
+
+
+
+
+
+
+	
+
+
+
---
+++ Project97/Project97/text.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//#include "main.c"
+#pragma warning(disable:4996)
+
+
+
+int main(void)
+{
+
+	extern char a;
+	printf("%c\n", a);
+
+    (void)msg();
+
+
+	return 0;
+
+}
+
+
+
+
+
+
+
