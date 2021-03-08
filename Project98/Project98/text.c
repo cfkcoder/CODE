e33b8c499a36d6944b19a0646a32196c81baaa17
cfkcoder <1261728635@qq.com>
2021-03-08 10:18:16 +0800

    Sleep(1000)

---
+++ Project98/Project98/text.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//#include <time.h>
+#include <Windows.h>
+#pragma warning(disable:4996)
+
+
+int main()
+{
+
+
+	//fflush(stdout);
+	
+	printf("hello");
+	Sleep(10000);
+	printf(" world");
+
+
+
+	return 0;
+
+
+}
