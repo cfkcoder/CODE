bc3a97061efa7ee0a63fedbf77a1a04a863ee533
cfkcoder <1261728635@qq.com>
2021-03-10 09:21:53 +0800

    scanf

---
+++ Project101/Project101/text.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main()
+{
+
+	int a, b;
+	scanf("%d%d", &a,&b);
+	printf("%d%d\n", a,b);
+
+
+	//int a;
+	//scanf("%d ", &a);
+	//printf("%d\n", a);
+
+
+	return 0;
+}
