af10b6ef24bea47d0c6af4b8405476b120c7f90a
cfkcoder <1261728635@qq.com>
2021-03-12 13:57:47 +0800

    exit(EXIT_FAILURE);

---
+++ Project102/Project102/text.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+
+#define N 10
+
+int main()
+{
+	
+
+	//exit(EXIT_FAILURE);
+	printf("%d\n", EXIT_FAILURE);
+
+
+	return 0;
+}
