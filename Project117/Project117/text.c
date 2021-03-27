85ea169e2daa36eff05172082c40c09dca414c1b
cfkcoder <1261728635@qq.com>
2021-03-27 09:33:06 +0800

    bool a=true

---
+++ Project117/Project117/text.c
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+
+
+
+
+int main()
+{
+
+
+	/*assert(0);
+	printf("666\n");*/
+	bool a = true;
+	bool b = false;
+	printf("%d\n", true);
+	printf("%d\n", false);
+
+	return 0;
+}
