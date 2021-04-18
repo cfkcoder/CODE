308ab4a4e33f56028d6393c090ad9228565b1306
cfkcoder <1261728635@qq.com>
2021-04-18 08:47:38 +0800

    char *s="";

---
+++ Project140/Project140/text.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning(disable:4996)
+
+
+//
+//void fun(char *s)
+//{
+//	printf("%s\n", s);
+//}
+//int main()
+//{
+//
+//
+//	fun("asdasdas");
+//	return 0;
+//}
+
+int main()
+{
+
+
+
+	char *s = "";
+	char arr[] = "aasdad";
+	
+	int n1 = strlen(s);
+	int n2 = strlen(arr);
+	printf("%d\n", n1);
+	printf("%d\n", n2);
+	printf("%s\n", arr+5);
+
+	/*char arr[] = "asdasdasd";
+	int n1 = strlen(arr);
+	printf("%d\n", n1);*/
+	printf("sss\n");
+
+	return 0;
+
+}
