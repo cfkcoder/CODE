bb4aed3588b99f9bf63c4c4ffd97502dd13d69d7
cfkcoder <1261728635@qq.com>
2021-04-19 10:53:39 +0800

    char *str="asdffggfhgfgh";

---
+++ Project141/Project141/text.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+
+int main()
+{
+
+	char *str = "asdasdasdas";
+	char *arr = "asdasdasdas";
+
+
+
+
+
+
+	printf("ssss");
+
+
+
+
+
+	return 0;
+}
