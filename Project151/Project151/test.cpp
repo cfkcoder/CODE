373c8e4112818c99a75b6fc1288880f484098e0d
cfkcoder <1261728635@qq.com>
2021-04-29 13:23:42 +0800

    namespace bit58

---
+++ Project151/Project151/test.cpp
@@ -0,0 +1,93 @@
+//#include <stdio.h>
+//#include <iostream>
+//using namespace std;
+//#pragma warning(disable:4996)
+
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+
+
+//#define N 10
+//
+//enum{
+//	N = 11
+//
+//};
+//
+//class Test{
+//
+//public:
+//	Test() 
+//	{
+//
+//	}
+//
+//private:
+//	int m_data;
+//	int m_value;
+//};
+//int main()
+//{
+//
+//	//Test();
+//	//Test a=Test();
+//	Test a(0);
+//	int b = int();
+//	cout << b << endl;
+////	Test b();
+//	return 0;
+//	
+//
+//
+//}
+
+//
+//void fun(int a,int b)
+//{
+//
+//
+//}
+//
+//void fun(double a, double b)
+//{
+//
+//}
+//int main()
+//{
+//
+//
+//	
+//	int a = 10;
+//	int b = 20;
+//	fun();
+//	
+//}
+
+
+#include <iostream>
+using namespace std;
+
+
+
+namespace bit58
+{
+	template <class _Ty>
+	class list
+	{
+
+	};
+}
+
+
+using namespace bit58;
+void main()
+{
+
+
+	list<int> mylist;
+
+
+
+
+}
