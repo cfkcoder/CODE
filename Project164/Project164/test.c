a108e2ca1d6d8b061ac801b6a68869c00d083365
cfkcoder <1261728635@qq.com>
2021-05-12 13:15:17 +0800

    while() ;

---
+++ Project164/Project164/test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+//#define N 10
+
+int main()
+{
+	int n = 10;
+	int i = 10;
+
+	while (n&&i)
+	
+		i--;
+	    n--;
+	
+	
+		
+
+		printf("%d ", i);
+		printf("%d ", n);
+
+
+
+
+
+
+
+	return 0;
+}
