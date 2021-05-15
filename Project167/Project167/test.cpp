1a0d7722d64960818b2962e93456e4ff25a3f676
cfkcoder <1261728635@qq.com>
2021-05-15 23:18:14 +0800

    Test a(a1);

---
+++ Project167/Project167/test.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+//
+//namespace bit58{
+//	class Test{
+//	public:
+//		Test(int d = 0) :_data(d)
+//		{
+//
+//
+//
+//
+//		}
+//		~Test()
+//		{
+//
+//
+//		}
+//		class Test1{
+//		public:
+//			Test1(int d1) :_data1(d1)
+//			{
+//
+//			}
+//		private:
+//			int _data1;
+//		};
+//
+//	private:
+//		int _data;
+//	};
+//
+//
+//}
+//	
+//
+//int main()
+//{
+//	bit58::Test::Test1 a;
+//
+//	return 0;
+//}
+//
+
+
+
+class Test{
+public:
+	Test(int d = 0) :_data(d)
+	{
+
+
+
+
+	}
+	~Test()
+	{
+
+
+	}
+private:
+	int _data;
+
+
+};
+
+
+
+int main()
+{
+
+	Test a;
+	Test a1(a);
+
+	return 0;
+
+}
