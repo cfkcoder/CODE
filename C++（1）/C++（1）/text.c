3388bbcfb33ad63f4e6a99a2c1b7fa3ee6cc7d49
cfkcoder <1261728635@qq.com>
2021-05-18 13:49:58 +0800

    int m_data=1;

---
+++ C++（1）/C++（1）/text.c
@@ -0,0 +1,51 @@
+//#include <iostream>
+//using namespace std;
+
+
+#include <stdio.h>
+#include <malloc.h>
+#pragma warning(disable:4996)
+
+//
+//class Test{
+//
+//public:
+//	Test(int a=0)
+//	{
+//		//m_data = data;
+//
+//	}
+//	~Test()
+//	{
+//
+//	}
+//
+//public:
+//
+//
+//private:
+//	int m_data=1;
+//	int m_value;
+//	//const int value;
+//};
+//
+//int main()
+//{
+//	Test a;
+//
+//	return 0;
+//}
+
+
+	
+int main()
+{
+
+	//int *p = (int*)malloc(40);
+	/*int *p;
+	*p = 100;
+*/
+	int a;
+	printf("%d\n", a);
+	return 0;
+}
