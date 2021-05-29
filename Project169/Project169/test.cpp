2a1b212d89209cdff0abbf405adc5b906b7b90c1
cfkcoder <1261728635@qq.com>
2021-05-29 08:40:58 +0800

    void fun(); chongzai

---
+++ Project169/Project169/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+void fun(int a=1, int b=2)
+{
+	int c = a + b;
+	printf("%d\n", c);
+
+}
+void fun()
+{
+
+}
+int main()
+{
+	int a = 10;
+	int b = 11; 
+	fun();
+
+
+}
