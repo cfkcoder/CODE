36e105a5f8a495a596ea138c9ff3450185e79c39
cfkcoder <1261728635@qq.com>
2021-06-06 22:32:46 +0800

    STL(1)

---
+++ C++ STL(1)/C++ STL(1)/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#pragma warning(disable:4996)
+
+
+
+namespace bit58
+{
+	template<class _Ty>
+	class list
+	{
+	public:
+		
+	private:
+
+	};
+}
+
+int main()
+{
+
+	bit58::list<int> mylist;
+
+
+	return 0;
+}
